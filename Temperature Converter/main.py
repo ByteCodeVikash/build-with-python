dd932368c1ef6d1ac948cc711112b6c7504dfb2d
ByteCodeVikash <vikash7618@gmail.com>
2026-04-22 10:17:01 +0530

    Temperature Converter

---
+++ Temperature Converter/main.py
@@ -0,0 +1,12 @@
+t=int(input("Enter temperature value: "))
+c=input("Enter unit C/F: ")
+
+
+if c=="C":
+   F = (t × 9/5) + 32
+   print(F)
+elif c == "F"
+   C = (t - 32) × 5/9 
+   print(C) 
+else:
+   print("Invalid")    
